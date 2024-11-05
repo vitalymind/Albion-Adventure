aedf395ad9ccb3a7a90ce93dda38192e97b564da
vitalymind <vitaly.mind@gmail.com>
2024-11-05 12:25:53 +0200

    add attack animation on LMB

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -4,7 +4,11 @@ class_name Player
 
 const SPEED = 0.8
 const ROTATION_SPEED = 12
+const ANIMATION_SPEED_SCALE = 1.5
 const JUMP_VELOCITY = 4.5
+const ATTACK_COOLDOWN = 1.1
+var attack_in_progress := false
+var attack_delay := 0.0
 var visual: Node3D
 var animation: AnimationPlayer
 var facing_direction := 1
@@ -13,8 +17,18 @@ func _ready() -> void:
 	visual = get_node("main_character_prototype")
 	animation = find_child("AnimationPlayer")
 	animation.get_animation("sword_idle_2").loop_mode = Animation.LOOP_LINEAR
-	animation.get_animation("sword_walk").loop_mode = Animation.LOOP_LINEAR
+	var walk_anim = animation.get_animation("sword_walk")
+	walk_anim.loop_mode = Animation.LOOP_LINEAR
+	animation.speed_scale = ANIMATION_SPEED_SCALE
 	animation.play("sword_idle_2")
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("action_attack"): attack()
+
+func _process(delta: float) -> void:
+	attack_delay = max(0, attack_delay - delta);
+	if attack_delay == 0:
+		attack_in_progress = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -37,18 +51,24 @@ func _physics_process(delta: float) -> void:
 	var move_dir := input[0] as int
 	
 	if move_dir == 0:
-		velocity.x = 0
-		# velocity.x = move_toward(velocity.x, 0, SPEED)
-		if animation.current_animation != "sword_idle_2":
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if !attack_in_progress and animation.current_animation != "sword_idle_2":
 			animation.play("sword_idle_2")
 	else:
-		velocity.x = move_dir * SPEED
-		if animation.current_animation != "sword_walk":
-			animation.play("sword_walk")
+		if !attack_in_progress:
+			velocity.x = move_dir * SPEED * ANIMATION_SPEED_SCALE
+			if animation.current_animation != "sword_walk":
+				animation.play("sword_walk")
 
-		# Change facing direction
-		facing_direction = move_dir
-			
-
-
+			# Change facing direction
+			facing_direction = move_dir
 	move_and_slide()
+
+func attack() -> void:
+	if attack_delay > 0:
+		return
+	
+	attack_in_progress = true
+	velocity.x = 0
+	attack_delay = ATTACK_COOLDOWN
+	animation.play("sword_slash")
